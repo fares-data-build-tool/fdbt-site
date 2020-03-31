bb2ceb14a468e96788184a1ecbb8d3b95840c1f7
Laurence Jones <laurence.jones@infinityworks.com>
2020-03-31 17:26:59 +0100

    [TFN-350] Added webpack config to enable hot reloading in docker, added dev dockerfile

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const withFonts = require('next-fonts');
 
 const nextConfig = {
     target: 'server',
+    webpackDevMiddleware: config => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        };
+
+        return config;
+    },
 };
 
 module.exports = withPlugins([[withFonts], [withImages], nextConfig]);
