3b60fc8eca1609e6d7fe198ce84309b16a95de51
Danny Davies <danny.davies@infinityworks.com>
2020-04-15 15:51:54 +0100

    Added error handling and cookies to pass info between pages and api, and added checks for valid user input

--- src/pages/api/howManyProducts.ts
+++ src/pages/api/howManyProducts.ts
@@ -1,47 +1,44 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NUMBER_OF_PRODUCTS_COOKIE } from '../../constants/index';
 import { getDomain, setCookieOnResponseObject, redirectToError, redirectTo } from './apiUtils';
+import { isSessionValid } from './service/validator';
+import { InputCheck } from '../howManyProducts';
 
-export const isNumberOfProductsInvalid = (req: NextApiRequest): boolean => {
-    const { numberOfProductsInput } = req.body;
-
-    // eslint-disable-next-line no-restricted-globals
-    if (isNaN(numberOfProductsInput)) {
-        return true;
+export const isNumberOfProductsInvalid = (req: NextApiRequest): InputCheck => {
+    const { numberOfProductsInput = '' } = req.body;
+    const inputAsNumber = Number(numberOfProductsInput);
+    let error;
+    if (numberOfProductsInput === '' || Number.isNaN(inputAsNumber)) {
+        error = 'Enter a number';
+    } else if (!Number.isInteger(inputAsNumber) || Number(inputAsNumber) > 10 || Number(inputAsNumber) < 1) {
+        error = 'Enter a whole number between 1 and 10';
+    } else {
+        error = '';
     }
-
-    if (!Number.isInteger(Number(numberOfProductsInput))) {
-        return true;
-    }
-
-    if (numberOfProductsInput > 20 || numberOfProductsInput < 1) {
-        return true;
-    }
-
-    return false;
+    const inputCheck = { numberOfProductsInput, error };
+    return inputCheck;
 };
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
     try {
-        if (!req.body.numberOfProductsInput) {
+        if (!isSessionValid(req, res)) {
+            throw new Error('Session is invalid.');
+        }
+
+        const userInputValidity = isNumberOfProductsInvalid(req);
+        if (userInputValidity.error === '') {
+            const numberOfProductsCookieValue = JSON.stringify({
+                numberOfProductsInput: userInputValidity.numberOfProductsInput,
+            });
+            setCookieOnResponseObject(getDomain(req), NUMBER_OF_PRODUCTS_COOKIE, numberOfProductsCookieValue, req, res);
+            redirectTo(res, '/productDetails');
+        } else {
+            const numberOfProductsCookieValue = JSON.stringify(userInputValidity);
+            setCookieOnResponseObject(getDomain(req), NUMBER_OF_PRODUCTS_COOKIE, numberOfProductsCookieValue, req, res);
             redirectTo(res, '/howManyProducts');
-            return;
         }
-
-        if (req.body.numberOfProductsInput === 0) {
-            throw new Error('0 farestages selected.');
-        }
-
-        if (isNumberOfProductsInvalid(req)) {
-            throw new Error('Invalid number of farestages selected.');
-        }
-
-        const numberOfProducts = req.body.numberOfProductsInput;
-        const cookieValue = JSON.stringify({ numberOfProducts });
-        setCookieOnResponseObject(getDomain(req), NUMBER_OF_PRODUCTS_COOKIE, cookieValue, req, res);
-        redirectTo(res, '/productDetails');
     } catch (error) {
-        const message = 'There was a problem inputting the number of fare stages:';
+        const message = 'There was a problem inputting the number of products:';
         redirectToError(res, message, error);
     }
 };
--- src/pages/howManyProducts.tsx
+++ src/pages/howManyProducts.tsx
@@ -1,10 +1,25 @@
+/* eslint-disable no-nested-ternary */
 import React, { ReactElement } from 'react';
+import { NextPageContext } from 'next';
+import { parseCookies } from 'nookies';
 import Layout from '../layout/Layout';
+import { NUMBER_OF_PRODUCTS_COOKIE, CSV_ZONE_UPLOAD_COOKIE } from '../constants';
+import { deleteCookieOnServerSide } from '../utils';
 
 const title = 'How Many Products - Fares data build tool';
 const description = 'How many products page of the Fares data build tool';
 
-const HowManyProducts = (): ReactElement => (
+export type InputCheck = {
+    error: string;
+    numberOfProductsInput: string;
+};
+
+type HowManyProductProps = {
+    previousPage: string;
+    inputCheck: InputCheck;
+};
+
+const HowManyProducts = ({ previousPage, inputCheck }: HowManyProductProps): ReactElement => (
     <Layout title={title} description={description}>
         <main className="govuk-main-wrapper app-main-class" id="main-content" role="main">
             <form action="/api/howManyProducts" method="post">
@@ -12,24 +27,37 @@ const HowManyProducts = (): ReactElement => (
                     <fieldset className="govuk-fieldset" aria-describedby="page-heading">
                         <legend className="govuk-fieldset__legend govuk-fieldset__legend--xl">
                             <h1 className="govuk-fieldset__heading" id="page-heading">
-                                How many products do you have for this zone or selected service?
+                                How many products do you have for{' '}
+                                {previousPage === 'csvZoneUpload'
+                                    ? 'this zone'
+                                    : previousPage === 'singleOperator'
+                                    ? 'your selected services'
+                                    : 'this zone or selected services'}
+                                ?
                             </h1>
                         </legend>
-
-                        <div className="govuk-form-group">
-                            <label className="govuk-hint" htmlFor="width-2">
-                                Enter the number of products associated with the zone or service selected.
+                        <div
+                            className={`govuk-form-group${
+                                inputCheck?.error ? ' govuk-form-group--error input-error' : ''
+                            }`}
+                        >
+                            <label className="govuk-label" htmlFor="numberOfProducts">
+                                Number of fare products (up to a maximum of 10)
                             </label>
+                            {inputCheck?.error ? (
+                                <span id="numberOfProducts-error" className="govuk-error-message">
+                                    <span className="govuk-visually-hidden">Error:</span> {inputCheck.error}
+                                </span>
+                            ) : null}
                             <input
-                                className="govuk-input govuk-input--width-2"
+                                className={`govuk-input govuk-input--width-2 ${
+                                    inputCheck?.error ? 'govuk-input--error' : ''
+                                }`}
                                 id="numberOfProducts"
                                 name="numberOfProductsInput"
-                                type="number"
-                                min="1"
-                                max="20"
-                                maxLength={2}
-                                required
-                                pattern="^[0-9]*$"
+                                type="text"
+                                defaultValue={!inputCheck?.error ? inputCheck?.numberOfProductsInput : ''}
+                                aria-describedby={inputCheck?.error ? `numberOfProducts-error` : ''}
                             />
                         </div>
                     </fieldset>
@@ -44,5 +72,29 @@ const HowManyProducts = (): ReactElement => (
         </main>
     </Layout>
 );
+
+const getPreviousPage = (cookies: { [key: string]: string }): string => {
+    let previousPage = 'unknown';
+    if (cookies[CSV_ZONE_UPLOAD_COOKIE]) {
+        previousPage = 'csvZoneUpload';
+    }
+    if (!cookies[CSV_ZONE_UPLOAD_COOKIE]) {
+        previousPage = 'singleOperator';
+    }
+    return previousPage;
+};
+
+export const getServerSideProps = (ctx: NextPageContext): {} => {
+    const cookies = parseCookies(ctx);
+    deleteCookieOnServerSide(ctx, NUMBER_OF_PRODUCTS_COOKIE);
+
+    const previousPage = getPreviousPage(cookies);
+    let inputCheck = {};
+    if (cookies[NUMBER_OF_PRODUCTS_COOKIE]) {
+        const numberOfProductsCookie = cookies[NUMBER_OF_PRODUCTS_COOKIE];
+        inputCheck = JSON.parse(numberOfProductsCookie);
+    }
+    return { props: { previousPage, inputCheck } };
+};
 
 export default HowManyProducts;
