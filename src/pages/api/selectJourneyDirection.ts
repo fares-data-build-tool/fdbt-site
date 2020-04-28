f1c07ec258ce080c406b2d30c68bc46fbe89066f
Tej Powar <tejwant.powar@infinityworks.com>
2020-04-28 15:51:56 +0100

    amended position of uuid retrieval for selectJourneyDirection page

--- src/pages/api/selectJourneyDirection.ts
+++ src/pages/api/selectJourneyDirection.ts
@@ -13,7 +13,13 @@ export default (req: NextApiRequest, res: NextApiResponse): void => {
         const { inboundJourney, outboundJourney } = req.body;
 
         if (inboundJourney && outboundJourney) {
-            const cookieValue = JSON.stringify({ errorMessages: [], inboundJourney, outboundJourney });
+            const uuid = getUuidFromCookie(req, res);
+
+            if (!uuid) {
+                throw new Error('No UUID found');
+            }
+
+            const cookieValue = JSON.stringify({ errorMessages: [], inboundJourney, outboundJourney, uuid });
             setCookieOnResponseObject(getDomain(req), JOURNEY_COOKIE, cookieValue, req, res);
             redirectTo(res, '/inputMethod');
         } else {
@@ -27,13 +33,7 @@ export default (req: NextApiRequest, res: NextApiResponse): void => {
                 errorMessages.push({ errorMessage: 'Choose an option for an outbound journey', id: outboundErrorId });
             }
 
-            const uuid = getUuidFromCookie(req, res);
-
-            if (!uuid) {
-                throw new Error('No UUID found');
-            }
-
-            const cookieValue = JSON.stringify({ errorMessages, inboundJourney, outboundJourney, uuid });
+            const cookieValue = JSON.stringify({ errorMessages, inboundJourney, outboundJourney });
             setCookieOnResponseObject(getDomain(req), JOURNEY_COOKIE, cookieValue, req, res);
             redirectTo(res, '/selectJourneyDirection');
         }
