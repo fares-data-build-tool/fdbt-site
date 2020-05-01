60a9ecc91f489b4ab9fa34bb5fe2483f4ba9e129
Tej Powar <tejwant.powar@infinityworks.com>
2020-05-01 14:20:44 +0100

    Added Error summary for the matching pages

--- src/components/matching/Matching.tsx
+++ src/components/matching/Matching.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from 'react';
+import ErrorSummary from '../ErrorSummary';
 import Layout from '../../layout/Layout';
 import MatchingList from '../MatchingList';
 import { UserFareStages } from '../../data/s3';
 import { Stop } from '../../data/auroradb';
 import { BasicService } from '../../interfaces/index';
+import { ErrorInfo } from '../../types';
 
 interface MatchingBaseProps {
     userFareStages: UserFareStages;
@@ -27,31 +29,45 @@ const MatchingBase = ({
     hintText,
     heading,
     apiEndpoint,
-}: MatchingBaseProps): ReactElement => (
-    <Layout title={title} description={description}>
-        <main className="govuk-main-wrapper app-main-class matching-page" id="main-content" role="main">
-            <form action={apiEndpoint} method="post">
-                <div className={`govuk-form-group${error ? ' govuk-form-group--error' : ''}`}>
-                    <legend className="govuk-fieldset__legend govuk-fieldset__legend--xl">
-                        <h1 className="govuk-fieldset__heading">{heading}</h1>
-                    </legend>
-                    <span id="dropdown-error" className="govuk-error-message">
-                        <span className={error ? '' : 'govuk-visually-hidden'}>
-                            Ensure each fare stage is assigned at least once.
-                        </span>
-                    </span>
-                    <span className="govuk-hint" id="match-fares-hint">
-                        {hintText}
-                    </span>
-                    <MatchingList userFareStages={userFareStages} stops={stops} />
-                </div>
+}: MatchingBaseProps): ReactElement => {
+    const errors: ErrorInfo[] = [];
 
-                <input type="hidden" name="service" value={JSON.stringify(service)} />
-                <input type="hidden" name="userfarestages" value={JSON.stringify(userFareStages)} />
-                <input type="submit" value="Continue" id="submit-button" className="govuk-button govuk-button--start" />
-            </form>
-        </main>
-    </Layout>
-);
+    if (error) {
+        errors.push({ errorMessage: 'Ensure each fare stage is assigned at least once.', id: 'dropdown-error' });
+    }
+
+    return (
+        <Layout title={title} description={description}>
+            <main className="govuk-main-wrapper app-main-class matching-page" id="main-content" role="main">
+                <form action={apiEndpoint} method="post">
+                    <ErrorSummary errors={errors} />
+                    <div className={`govuk-form-group${error ? ' govuk-form-group--error' : ''}`}>
+                        <legend className="govuk-fieldset__legend govuk-fieldset__legend--xl">
+                            <h1 className="govuk-fieldset__heading">{heading}</h1>
+                        </legend>
+                        <span id="dropdown-error" className="govuk-error-message">
+                            <span className={error ? '' : 'govuk-visually-hidden'}>
+                                Ensure each fare stage is assigned at least once.
+                            </span>
+                        </span>
+                        <span className="govuk-hint" id="match-fares-hint">
+                            {hintText}
+                        </span>
+                        <MatchingList userFareStages={userFareStages} stops={stops} />
+                    </div>
+
+                    <input type="hidden" name="service" value={JSON.stringify(service)} />
+                    <input type="hidden" name="userfarestages" value={JSON.stringify(userFareStages)} />
+                    <input
+                        type="submit"
+                        value="Continue"
+                        id="submit-button"
+                        className="govuk-button govuk-button--start"
+                    />
+                </form>
+            </main>
+        </Layout>
+    );
+};
 
 export default MatchingBase;
