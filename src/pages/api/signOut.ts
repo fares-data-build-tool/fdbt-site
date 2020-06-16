d94140e0cdea99328f17392da60802c6996a0d3d
gilesdrinkall <giles.drinkall@infinityworks.com>
2020-06-16 17:17:47 +0100

    Imported the getAttributeFromIdToken function and  added a try catch

--- src/pages/api/signOut.ts
+++ src/pages/api/signOut.ts
@@ -1,28 +1,17 @@
-import Cookies from 'cookies';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { decode } from 'jsonwebtoken';
-import { redirectToError, redirectTo, signOutUser } from './apiUtils';
-import { ID_TOKEN_COOKIE } from '../../constants';
-import { CognitoIdToken } from '../../interfaces';
+import { redirectToError, redirectTo, signOutUser, getAttributeFromIdToken } from './apiUtils';
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
     try {
-        const cookies = new Cookies(req, res);
-        const idToken = cookies.get(ID_TOKEN_COOKIE) ?? null;
-        if (!idToken) {
-            redirectTo(res, '/login');
-            return;
+        const username = getAttributeFromIdToken(req, res, 'cognito:username');
+
+        try {
+            await signOutUser(username, req, res);
+            redirectTo(res, '/');
+        } catch (error) {
+            console.error(`failed to sign out user: ${error.stack}`);
+            redirectTo(res, '/');
         }
-        const decodedToken = decode(idToken) as CognitoIdToken;
-
-        const username = decodedToken?.['cognito:username'] ?? null;
-
-        await signOutUser(username, req, res)
-            .then(() => redirectTo(res, '/'))
-            .catch(error => {
-                console.error(`failed to sign out user: ${error.stack}`);
-                redirectTo(res, '/');
-            });
     } catch (error) {
         const message = 'There was a problem signing out of your account';
         redirectToError(res, message, error);
