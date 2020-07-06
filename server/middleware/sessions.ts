c3a8730e2dc2f7eec03210e61927e729ff34597e
Laurence Jones <laurence.jones@infinityworks.com>
2020-07-06 08:32:28 +0100

    fix: set region for aws sdk commands

--- server/middleware/sessions.ts
+++ server/middleware/sessions.ts
@@ -1,13 +1,17 @@
 import { Express } from 'express';
-import AWS, { DynamoDB } from 'aws-sdk';
+import AWS from 'aws-sdk';
 import session, { SessionOptions } from 'express-session';
 import connectDynamoDb from 'connect-dynamodb';
 
 export default (server: Express): void => {
     const DynamoDbStore = connectDynamoDb(session);
 
-    const dynamoDbOptions: { table: string; client?: DynamoDB } = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const dynamoDbOptions: any = {
         table: 'sessions',
+        AWSConfigJSON: {
+            region: 'eu-west-2',
+        },
     };
 
     if (process.env.NODE_ENV === 'development') {
