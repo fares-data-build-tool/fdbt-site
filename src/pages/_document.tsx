6c8aa49b82e23effb39aa03b45df115d9313c94d
Tej Powar <tej@powtech.co.uk>
2020-07-21 10:11:18 +0100

    updated google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,23 +46,23 @@ class MyDocument extends Document<DocumentProps> {
         return (
             <Html lang="en" className="govuk-template app-html-class flexbox no-flexboxtweener">
                 <Head nonce={this.props.nonce}>
-                    {process.env.NODE_ENV === 'production' && (
-                        <script
-                            async
-                            src="https://www.googletagmanager.com/gtag/js?id=UA-173062045-1"
-                            nonce={this.props.nonce}
-                        />
-                    )}
-                    {process.env.NODE_ENV === 'production' && (
-                        <script
-                            nonce={this.props.nonce}
-                            dangerouslySetInnerHTML={{
-                                __html: `window.dataLayer = window.dataLayer || [];
+                    {process.env.STAGE === 'prod' && (
+                        <>
+                            <script
+                                async
+                                src="https://www.googletagmanager.com/gtag/js?id=UA-173062045-1"
+                                nonce={this.props.nonce}
+                            />
+                            <script
+                                nonce={this.props.nonce}
+                                dangerouslySetInnerHTML={{
+                                    __html: `window.dataLayer = window.dataLayer || [];
                                         function gtag(){dataLayer.push(arguments);}
                                         gtag('js', new Date());
                                         gtag('config', 'UA-173062045-1');`,
-                            }}
-                        />
+                                }}
+                            />
+                        </>
                     )}
                 </Head>
                 <body className="govuk-template__body app-body-class js-enabled">
