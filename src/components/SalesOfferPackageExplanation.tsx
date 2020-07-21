83e9de288df7c03a2f279ae0c058af1a9ff84fda
Danny Davies <danny.davies@infinityworks.com>
2020-07-21 11:48:27 +0100

    Fixed sales offer package explanation

--- src/components/SalesOfferPackageExplanation.tsx
+++ src/components/SalesOfferPackageExplanation.tsx
@@ -3,11 +3,11 @@ import React, { ReactElement } from 'react';
 export const SalesOfferPackageExplanation = (): ReactElement => (
     <>
         <h1 className="govuk-heading-s">What is a Sales Offer Package?</h1>
-        <p className="govuk-body">To create a NeTEx for your fares it needs to contain the following:</p>
+        <p className="govuk-body">To create NeTEx for your fares, you need to provide the following:</p>
         <ol className="govuk-body">
-            <li>Where the ticket can be bought</li>
-            <li>What payment methods can be used to buy the ticket</li>
-            <li>In what format the ticket will be used by passengers</li>
+            <li>Where a ticket can be bought</li>
+            <li>What payment methods can be used to buy a ticket</li>
+            <li>In what format a ticket will be used by passengers</li>
         </ol>
         <p className="govuk-body">
             This combination of information is known as a <b>sales offer package</b>.
