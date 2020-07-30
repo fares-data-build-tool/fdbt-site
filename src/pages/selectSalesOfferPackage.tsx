c43a486a8678a0640ed2376cf57c3838ccfa6699
Rob Catton <robmcatton@gmail.com>
2020-07-30 12:35:55 +0100

    Removed optionality from index in generate checkbox method

--- src/pages/selectSalesOfferPackage.tsx
+++ src/pages/selectSalesOfferPackage.tsx
@@ -58,7 +58,7 @@ export interface SelectSalesOfferPackageProps {
 
 const generateCheckbox = (
     salesOfferPackagesList: SalesOfferPackage[],
-    productIndex?: number,
+    productIndex: number,
     productName?: string,
     selected?: { [key: string]: string },
 ): ReactElement[] => {
