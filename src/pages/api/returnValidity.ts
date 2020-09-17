29d592a0f722cd02528c7260bbdef2d69aa2753c
Danny Davies <danny.davies@infinityworks.com>
2020-09-17 11:15:07 +0100

    Added tests for returnValidity API

--- src/pages/api/returnValidity.ts
+++ src/pages/api/returnValidity.ts
@@ -14,63 +14,69 @@ const monthsAmountError = 'Enter a number of months between 1 and 72';
 const yearsAmountError = 'Enter a number of years greater than 1';
 const selectError = 'Choose one of the options from the dropdown list';
 
-export const returnValiditySchema = yup.object({
-    validity: yup
-        .string()
-        .oneOf(['Yes', 'No'])
-        .required(radioButtonError),
-    amount: yup
-        .mixed()
-        .when('validity', {
-            is: 'No',
-            then: yup.string().notRequired(),
-        })
-        .when('validity', {
+export const returnValiditySchema = yup
+    .object({
+        validity: yup
+            .string()
+            .oneOf(['Yes', 'No'])
+            .required(radioButtonError),
+        amount: yup
+            .mixed()
+            .when('validity', {
+                is: 'No',
+                then: yup.string().notRequired(),
+            })
+            .when('validity', {
+                is: 'Yes',
+                then: yup
+                    .number()
+                    .typeError(inputError)
+                    .when('duration', {
+                        is: 'days',
+                        then: yup
+                            .number()
+                            .typeError(inputError)
+                            .integer(inputError)
+                            .min(1, daysAmountError)
+                            .max(31, daysAmountError),
+                    })
+                    .when('duration', {
+                        is: 'weeks',
+                        then: yup
+                            .number()
+                            .typeError(inputError)
+                            .integer(inputError)
+                            .min(1, weeksAmountError)
+                            .max(52, weeksAmountError),
+                    })
+                    .when('duration', {
+                        is: 'months',
+                        then: yup
+                            .number()
+                            .typeError(inputError)
+                            .integer(inputError)
+                            .min(1, monthsAmountError)
+                            .max(72, monthsAmountError),
+                    })
+                    .when('duration', {
+                        is: 'years',
+                        then: yup
+                            .number()
+                            .typeError(inputError)
+                            .integer(inputError)
+                            .min(1, yearsAmountError),
+                    })
+                    .required(inputError),
+            }),
+        duration: yup.string().when('validity', {
             is: 'Yes',
             then: yup
-                .number()
-                .typeError(inputError)
-                .when('duration', {
-                    is: 'days',
-                    then: yup
-                        .number()
-                        .typeError(inputError)
-                        .min(1, daysAmountError)
-                        .max(31, daysAmountError),
-                })
-                .when('duration', {
-                    is: 'weeks',
-                    then: yup
-                        .number()
-                        .typeError(inputError)
-                        .min(1, weeksAmountError)
-                        .max(52, weeksAmountError),
-                })
-                .when('duration', {
-                    is: 'months',
-                    then: yup
-                        .number()
-                        .typeError(inputError)
-                        .min(1, monthsAmountError)
-                        .max(72, monthsAmountError),
-                })
-                .when('duration', {
-                    is: 'years',
-                    then: yup
-                        .number()
-                        .typeError(inputError)
-                        .min(1, yearsAmountError),
-                })
-                .required(inputError),
+                .string()
+                .oneOf(['days', 'weeks', 'months', 'years'], selectError)
+                .required(selectError),
         }),
-    duration: yup.string().when('validity', {
-        is: 'Yes',
-        then: yup
-            .string()
-            .oneOf(['days', 'weeks', 'months', 'years'], selectError)
-            .required(selectError),
-    }),
-});
+    })
+    .required();
 
 export const getErrorIdFromValidityError = (errorPath: string): string => {
     switch (errorPath) {
@@ -102,7 +108,7 @@ export default async (req: NextApiRequestWithSession, res: NextApiResponse): Pro
             errors = validityErrors.inner.map(error => ({
                 id: getErrorIdFromValidityError(error.path),
                 errorMessage: error.message,
-                userInput: error.value,
+                userInput: String(error.value),
             }));
         }
 
@@ -113,7 +119,7 @@ export default async (req: NextApiRequestWithSession, res: NextApiResponse): Pro
         }
 
         updateSessionAttribute(req, RETURN_VALIDITY_ATTRIBUTE, { amount, duration });
-        redirectTo(res, '/returnValidity');
+        redirectTo(res, '/selectSalesOfferPackage');
     } catch (error) {
         const message = 'There was a problem in the returnValidity API.';
         redirectToError(res, message, 'api.returnValidity', error);
---
+++ tests/pages/api/returnValidity.test.ts
@@ -0,0 +1,114 @@
+import { getMockRequestAndResponse } from '../../testData/mockData';
+import returnValidity, {
+    returnValiditySchema,
+    getErrorIdFromValidityError,
+} from '../../../src/pages/api/returnValidity';
+import * as sessions from '../../../src/utils/sessions';
+import { RETURN_VALIDITY_ATTRIBUTE } from '../../../src/constants';
+import { ErrorInfo } from '../../../src/interfaces';
+
+describe('returnValidity', () => {
+    const writeHeadMock = jest.fn();
+    const updateSessionAttributeSpy = jest.spyOn(sessions, 'updateSessionAttribute');
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    describe('returnValiditySchema', () => {
+        it.each([
+            [{}, false],
+            [{ validity: 'No' }, true],
+            [{ validity: 'Yes' }, false],
+            [{ validity: 'Yes', amount: '5' }, false],
+            [{ validity: 'Yes', duration: 'days' }, false],
+            [{ validity: 'Yes', amount: '7', duration: 'days' }, true],
+            [{ validity: 'Yes', amount: 'abc', duration: 'days' }, false],
+            [{ validity: 'Yes', amount: '   ', duration: 'days' }, false],
+            [{ validity: 'Yes', amount: '0', duration: 'days' }, false],
+            [{ validity: 'Yes', amount: '-17', duration: 'days' }, false],
+            [{ validity: 'Yes', amount: '1.74', duration: 'days' }, false],
+            [{ validity: 'Yes', amount: '6', duration: 'hello there' }, false],
+            [{ validity: 'Yes', amount: '31', duration: 'days' }, true],
+            [{ validity: 'Yes', amount: '32', duration: 'days' }, false],
+            [{ validity: 'Yes', amount: '53', duration: 'weeks' }, false],
+            [{ validity: 'Yes', amount: '52', duration: 'weeks' }, true],
+            [{ validity: 'Yes', amount: '73', duration: 'months' }, false],
+            [{ validity: 'Yes', amount: '72', duration: 'months' }, true],
+            [{ validity: 'Yes', amount: '5', duration: 'years' }, true],
+        ])('should validate that %s is %s', (candidate, validity) => {
+            const result = returnValiditySchema.isValidSync(candidate);
+            expect(result).toEqual(validity);
+        });
+    });
+
+    describe('getErrorIdFromValidityError', () => {
+        it.each([
+            ['return-validity-defined', 'validity'],
+            ['return-validity-amount', 'amount'],
+            ['return-validity-units', 'duration'],
+        ])('should return the id as %s when the error path is %s', (expectedId, errorPath) => {
+            const actualId = getErrorIdFromValidityError(errorPath);
+            expect(actualId).toEqual(expectedId);
+        });
+
+        it('should throw an error when the error path does not match a valid input field', () => {
+            expect(() => getErrorIdFromValidityError('notValid')).toThrow();
+        });
+    });
+
+    it('should throw an error and redirect to the error page when the session is invalid', async () => {
+        const { req, res } = getMockRequestAndResponse({
+            cookieValues: { operator: null },
+            mockWriteHeadFn: writeHeadMock,
+        });
+        await returnValidity(req, res);
+        expect(writeHeadMock).toBeCalledWith(302, {
+            Location: '/error',
+        });
+    });
+
+    it('should set the RETURN_VALIDITY_ATTRIBUTE and redirect to /selectSalesOfferPackage when no errors are found', async () => {
+        const mockPassengerTypeDetails = {
+            amount: '6',
+            duration: 'weeks',
+        };
+        const mockBody = {
+            ...mockPassengerTypeDetails,
+            validity: 'Yes',
+        };
+        const { req, res } = getMockRequestAndResponse({
+            body: mockBody,
+            mockWriteHeadFn: writeHeadMock,
+        });
+        await returnValidity(req, res);
+        expect(updateSessionAttributeSpy).toBeCalledWith(req, RETURN_VALIDITY_ATTRIBUTE, mockPassengerTypeDetails);
+        expect(writeHeadMock).toBeCalledWith(302, {
+            Location: '/selectSalesOfferPackage',
+        });
+    });
+
+    it('should set the RETURN_VALIDITY_ATTRIBUTE with errors and redirect to itself (i.e. /returnValidity) when there are errors present', async () => {
+        const mockPassengerTypeDetails = {
+            validity: 'Yes',
+            amount: '54',
+            duration: 'days',
+        };
+        const mockErrors: ErrorInfo[] = [
+            {
+                errorMessage: 'Enter a number of days between 1 and 31',
+                id: 'return-validity-amount',
+                userInput: '54',
+            },
+        ];
+        const { req, res } = getMockRequestAndResponse({
+            body: mockPassengerTypeDetails,
+            mockWriteHeadFn: writeHeadMock,
+        });
+        await returnValidity(req, res);
+        expect(updateSessionAttributeSpy).toBeCalledWith(req, RETURN_VALIDITY_ATTRIBUTE, { errors: mockErrors });
+        expect(writeHeadMock).toBeCalledWith(302, {
+            Location: '/returnValidity',
+        });
+    });
+});
