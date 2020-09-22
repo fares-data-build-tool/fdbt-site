bf4bc2089277a8d92e3b7f2b47e00dd4a29bbf83
Danny Davies <danny.davies@infinityworks.com>
2020-09-22 18:25:26 +0100

    Added full cookies preferences page

--- src/pages/cookies.tsx
+++ src/pages/cookies.tsx
@@ -1,12 +1,101 @@
 import React, { ReactElement } from 'react';
+import CsrfForm from '../components/CsrfForm';
+import { CustomAppProps } from '../interfaces';
 import { TwoThirdsLayout } from '../layout/Layout';
 
-const title = 'Cookie on the Fares Data Build Tool';
+const title = 'Cookies on the Fares Data Build Tool';
 const description = 'Cookies Preferences page of the Fares Data Build Tool';
 
-const Cookies = (): ReactElement => (
+const Cookies = ({ csrfToken }: CustomAppProps): ReactElement => (
     <TwoThirdsLayout title={title} description={description}>
-        <h1 className="govuk-heading-l">Cookies on the Fares Data Build Tool</h1>
+        <div className="govuk-!-margin-bottom-8">
+            <h1 className="govuk-heading-l">Cookies on the Fares Data Build Tool</h1>
+        </div>
+        <p className="govuk-body">
+            Cookies are files saved on your phone, tablet or computer when you visit a website.
+        </p>
+        <p className="govuk-body">
+            We use cookies to store information about how you use the Fares Data Build Tool website, such as the pages
+            you visit.
+        </p>
+        <h2 className="govuk-heading-m govuk-!-margin-bottom-3">Cookie Settings</h2>
+        <div className="cookie-settings__form-wrapper">
+            <p className="govuk-body">
+                We use 2 types of cookie. You can choose which cookies you&apos;re happy for us to use.
+            </p>
+            <CsrfForm action="/api/cookies" method="post" csrfToken={csrfToken}>
+                <>
+                    <div className="govuk-form-group">
+                        <fieldset className="govuk-fieldset" aria-describedby="analytics-cookies-hint">
+                            <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">
+                                <h2 className="govuk-fieldset__heading">Cookies that measure website use</h2>
+                            </legend>
+                            <div id="analytics-cookies-hint" className="govuk-hint govuk-!-margin-bottom-3">
+                                <p className="govuk-body govuk-hint">
+                                    We use Google Analytics to measure how you use the website so we can improve it
+                                    based on user needs. We do not allow Google to use or share the data about how you
+                                    use this site.
+                                </p>
+                                <p className="govuk-body govuk-hint">
+                                    Google Analytics sets cookies that store anonymised information about:
+                                </p>
+                                <ul className="govuk-list govuk-list--bullet govuk-hint">
+                                    <li>how you got to the site</li>
+                                    <li>the pages you visit on the Fares Data Build Tool, and how long each page</li>
+                                    <li>what you click on while you&apos;re visiting the site</li>
+                                </ul>
+                            </div>
+                            <div className="govuk-radios">
+                                <div className="govuk-radios__item">
+                                    <input
+                                        className="govuk-radios__input"
+                                        type="radio"
+                                        id="accept-analytics-cookies"
+                                        name="analyticsCookies"
+                                        value="on"
+                                    />
+                                    <label
+                                        className="govuk-label govuk-radios__label"
+                                        htmlFor="accept-analytics-cookies"
+                                    >
+                                        Use cookies that measure my website use
+                                    </label>
+                                </div>
+                                <div className="govuk-radios__item">
+                                    <input
+                                        className="govuk-radios__input"
+                                        type="radio"
+                                        id="decline-analytics-cookies"
+                                        name="analyticsCookies"
+                                        value="off"
+                                    />
+                                    <label
+                                        className="govuk-label govuk-radios__label"
+                                        htmlFor="decline-analytics-cookies"
+                                    >
+                                        Do not use cookies that measure my website use
+                                    </label>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+                    <div>
+                        <h2 className="govuk-heading-m">Strictly necessary cookies</h2>
+                        <p className="govuk-body">These essential cookies are used to remember your progress.</p>
+                        <p className="govuk-body">They always need to be on.</p>
+                        <p className="govuk-body">
+                            <a href="/cookiePolicy">Find out more about our usage of cookies in our cookie policy</a>
+                        </p>
+                    </div>
+                    <input
+                        type="submit"
+                        value="Save Changes"
+                        id="save-cookie-preferences-button"
+                        className="govuk-button"
+                    />
+                </>
+            </CsrfForm>
+        </div>
     </TwoThirdsLayout>
 );
 
