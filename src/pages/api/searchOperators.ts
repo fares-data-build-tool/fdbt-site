fc94ac1952caf7e07979cb0173b8a998e924a950
Tej Powar <tej@powtech.co.uk>
2020-09-30 20:52:49 +0100

    error handling when no entry

---
+++ src/pages/api/searchOperators.ts
@@ -0,0 +1,38 @@
+import { NextApiResponse } from 'next';
+import { ErrorInfo, NextApiRequestWithSession } from '../../interfaces';
+import { SEARCH_OPERATOR_ATTRIBUTE } from '../../constants';
+import { redirectTo, redirectToError } from './apiUtils';
+import { updateSessionAttribute } from '../../utils/sessions';
+
+export interface SearchOperators {
+    searchResults: string[];
+}
+
+export interface SearchOperatorsWithErrors extends SearchOperators {
+    errors: ErrorInfo[];
+}
+
+export default (req: NextApiRequestWithSession, res: NextApiResponse): void => {
+    const errors: ErrorInfo[] = [];
+
+    try {
+        const { searchText } = req.body;
+
+        if (searchText.length < 4) {
+            errors.push({
+                errorMessage: 'Search requires a minimum of three characters',
+                id: 'searchText',
+            });
+        }
+
+        if (errors.length > 0) {
+            updateSessionAttribute(req, SEARCH_OPERATOR_ATTRIBUTE, { errors, searchResults: [] });
+            redirectTo(res, '/searchOperators');
+        }
+
+        redirectTo(res, `/searchOperators?search=${searchText}`);
+    } catch (err) {
+        const message = 'There was a problem in the search operators api.';
+        redirectToError(res, message, 'api.searchOperators', err);
+    }
+};
--- src/pages/searchOperators.tsx
+++ src/pages/searchOperators.tsx
@@ -1,8 +1,12 @@
 import React, { ReactElement } from 'react';
 import TwoThirdsLayout from '../layout/Layout';
-import { CustomAppProps, ErrorInfo } from '../interfaces';
+import { CustomAppProps, ErrorInfo, NextPageContextWithSession } from '../interfaces';
 import CsrfForm from '../components/CsrfForm';
 import ErrorSummary from '../components/ErrorSummary';
+import FormElementWrapper from '../components/FormElementWrapper';
+import { getSessionAttribute } from '../utils/sessions';
+import { SEARCH_OPERATOR_ATTRIBUTE } from '../constants';
+import { isSearchOperatorAttributeWithErrors } from '../interfaces/typeGuards';
 
 const title = 'Search Operators - Fares Data Build Tool';
 const description = 'Search Operators page for the Fares Data Build Tool';
@@ -22,20 +26,40 @@ const SearchOperators = ({ errors = [], csrfToken }: SearchOperatorProps & Custo
                             Search for the operators that the ticket covers
                         </label>
                     </h1>
-                    <input
-                        className="govuk-input govuk-!-width-three-quarters"
-                        id="event-name"
-                        name="event-name"
-                        type="text"
-                    />
-                    <input type="submit" value="Search" id="search-button" className="govuk-button govuk-!-margin-left-5" />
+                    <FormElementWrapper errors={errors} errorId="searchText" errorClass="govuk-input--error">
+                        <>
+                            <input
+                                className="govuk-input govuk-!-width-three-quarters"
+                                id="searchText"
+                                name="searchText"
+                                type="text"
+                            />
+                            <input
+                                type="submit"
+                                value="Search"
+                                id="search-button"
+                                className="govuk-button govuk-!-margin-left-5"
+                            />
+                        </>
+                    </FormElementWrapper>
                 </div>
             </>
         </CsrfForm>
     </TwoThirdsLayout>
 );
 
-export const getServerSideProps = (): { props: SearchOperatorProps } => {
+export const getServerSideProps = (ctx: NextPageContextWithSession): { props: SearchOperatorProps } => {
+    const searchOperatorsAttribute = getSessionAttribute(ctx.req, SEARCH_OPERATOR_ATTRIBUTE);
+
+    if (isSearchOperatorAttributeWithErrors(searchOperatorsAttribute) && searchOperatorsAttribute.errors.length > 0) {
+        const { errors } = searchOperatorsAttribute;
+        return {
+            props: {
+                errors,
+            },
+        };
+    }
+
     return { props: { errors: [] } };
 };
 
