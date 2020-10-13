6049e2a91e7e506eaf2d7ae2b18d4900d88a7541
Tej Powar <tej@powtech.co.uk>
2020-10-13 11:59:18 +0100

    refactor of searchoperator

--- src/pages/api/searchOperators.ts
+++ src/pages/api/searchOperators.ts
@@ -3,13 +3,41 @@ import { ErrorInfo, NextApiRequestWithSession } from '../../interfaces';
 import { SEARCH_OPERATOR_ATTRIBUTE } from '../../constants';
 import { redirectTo, redirectToError } from './apiUtils';
 import { getSessionAttribute, updateSessionAttribute } from '../../utils/sessions';
-import { removeExcessWhiteSpace } from './apiUtils/validator';
 import { OperatorNameType } from '../../data/auroradb';
 
 export interface SearchOperators {
     selectedOperators: OperatorNameType[];
     errors: ErrorInfo[];
 }
+
+export const populateOperatorsFromRequest = (
+    requestBody: { [key: string]: string },
+    operators: OperatorNameType[],
+): OperatorNameType[] => {
+    const body: { [key: string]: string | string[] } = requestBody;
+
+    const selectedOperators: OperatorNameType[] = [];
+
+    Object.entries(body).forEach(entry => {
+        if (entry[0] === 'searchText' || entry[0] === 'addOperator') {
+            return;
+        }
+
+        if (!operators.some(operator => operator.nocCode === entry[1].toString())) {
+            selectedOperators.push({
+                operatorPublicName: entry[0].toString(),
+                nocCode: entry[1].toString(),
+            });
+        }
+    });
+
+    return selectedOperators;
+};
+
+export const checkValidOperatorEntry = (searchText: string): boolean => {
+    const searchRegex = new RegExp(/^[a-zA-Z0-9\-:\s]+$/g);
+    return searchRegex.test(searchText);
+};
 
 export default (req: NextApiRequestWithSession, res: NextApiResponse): void => {
     const errors: ErrorInfo[] = [];
@@ -20,9 +48,22 @@ export default (req: NextApiRequestWithSession, res: NextApiResponse): void => {
 
     const searchOperatorAttribute = getSessionAttribute(req, SEARCH_OPERATOR_ATTRIBUTE);
 
-    if (addOperator) {
-        const requestBody: { [key: string]: string | string[] } = req.body;
+    const { searchText } = req.body;
 
+    if (!checkValidOperatorEntry(searchText)) {
+        errors.push({
+            errorMessage: 'Search must only include letters a to z, hyphens, spaces or colons',
+            id: 'searchText',
+        });
+
+        updateSessionAttribute(req, SEARCH_OPERATOR_ATTRIBUTE, {
+            errors,
+            selectedOperators: searchOperatorAttribute?.selectedOperators || [],
+        });
+        redirectTo(res, `/searchOperators`);
+    }
+
+    if (addOperator) {
         if (Object.keys(req.body).length - 2 === 0) {
             errors.push({
                 errorMessage: 'Choose at least one operator from the options',
@@ -36,26 +77,9 @@ export default (req: NextApiRequestWithSession, res: NextApiResponse): void => {
             redirectTo(res, `/searchOperators?${queryString}`);
         }
 
-        const selectedOperators: OperatorNameType[] = [];
-
         const currentSelectedOperators = (searchOperatorAttribute && searchOperatorAttribute.selectedOperators) || [];
 
-        Object.entries(requestBody).forEach(entry => {
-            if (entry[0] === 'searchText' || entry[0] === 'addOperator') {
-                return;
-            }
-
-            if (
-                !searchOperatorAttribute?.selectedOperators.some(
-                    operator => operator.nocCode === removeExcessWhiteSpace(entry[1].toString()),
-                )
-            ) {
-                selectedOperators.push({
-                    operatorPublicName: entry[0].toString(),
-                    nocCode: removeExcessWhiteSpace(entry[1].toString()),
-                });
-            }
-        });
+        const selectedOperators: OperatorNameType[] = populateOperatorsFromRequest(req.body, currentSelectedOperators);
 
         const mergedArray = selectedOperators.concat(currentSelectedOperators);
 
@@ -68,7 +92,7 @@ export default (req: NextApiRequestWithSession, res: NextApiResponse): void => {
     }
 
     try {
-        const refinedSearch = removeExcessWhiteSpace(req.body.searchText);
+        const refinedSearch = req.body.searchText;
 
         if (refinedSearch.length < 3) {
             errors.push({
@@ -81,7 +105,7 @@ export default (req: NextApiRequestWithSession, res: NextApiResponse): void => {
             });
             redirectTo(res, '/searchOperators');
         }
-        // below session attribute arguably shouldnt be set, and if it is, doesnt need an empty array inside it.
+
         updateSessionAttribute(req, SEARCH_OPERATOR_ATTRIBUTE, {
             errors,
             selectedOperators: searchOperatorAttribute?.selectedOperators || [],
