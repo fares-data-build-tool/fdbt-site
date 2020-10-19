28a717acbc18d7219fefa1fd43a172b64ccf9f9c
Danny Davies <danny.davies@infinityworks.com>
2020-10-19 15:32:00 +0100

    Added new cookie banner button

--- src/layout/CookieBanner.tsx
+++ src/layout/CookieBanner.tsx
@@ -1,4 +1,9 @@
 import React, { FC } from 'react';
+import axios from 'axios';
+
+const handleAcceptAllClick = async (): Promise<void> => {
+    await axios.post('/api/cookies', { tracking: 'on' });
+};
 
 const CookieBanner: FC = () => (
     <div id="global-cookie-message" className="cookie-banner" role="region" aria-label="cookie banner">
@@ -16,7 +21,17 @@ const CookieBanner: FC = () => (
                             work as well as possible and to improve the service.
                         </p>
                     </div>
-                    <div className="cookie-banner__button">
+                    <div className="cookie-banner__button govuk-grid-column-full govuk-grid-column-one-half-from-desktop">
+                        <button
+                            id="accept-all-cookies-link"
+                            className="govuk-button cookie-banner__button--inline"
+                            type="submit"
+                            onClick={async (): Promise<void> => handleAcceptAllClick()}
+                        >
+                            Accept all cookies
+                        </button>
+                    </div>
+                    <div className="cookie-banner__button govuk-grid-column-full govuk-grid-column-one-half-from-desktop">
                         <a
                             id="set-cookie-preferences-link"
                             className="govuk-button cookie-banner__button--inline"
