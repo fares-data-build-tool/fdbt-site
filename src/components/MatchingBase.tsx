2ea1dc3de69ac6fa352006811c78c619493de4ab
Tej Powar <tej@powtech.co.uk>
2020-11-06 11:40:11 +0000

    WIP

--- src/components/MatchingBase.tsx
+++ src/components/MatchingBase.tsx
@@ -3,10 +3,11 @@ import ErrorSummary from './ErrorSummary';
 import FormElementWrapper from './FormElementWrapper';
 import { FullColumnLayout } from '../layout/Layout';
 import MatchingList from './MatchingList';
-import { UserFareStages } from '../data/s3';
+import { FareStage, UserFareStages } from '../data/s3';
 import { Stop } from '../data/auroradb';
 import { BasicService, ErrorInfo } from '../interfaces';
 import CsrfForm from './CsrfForm';
+import { formatStopName } from 'src/utils';
 
 interface MatchingBaseProps {
     userFareStages: UserFareStages;
@@ -53,12 +54,15 @@ const MatchingBase = ({
     // }, [selectedOption]);
 
     const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+
         e.preventDefault();
+        console.log("selectedd here", selectedOption);
         setAutoPopulate(true);
     };
 
-    const selectedValue = useCallback(
+    const selectedValueCallback = useCallback(
         (event: React.ChangeEvent<HTMLSelectElement>): void => {
+            console.log('the callback');
             if (event.target.value === 'notApplicable') {
                 const index = selectedOption.findIndex(option => {
                     return option.id === event.target.id;
@@ -82,6 +86,77 @@ const MatchingBase = ({
         [selectedOption],
     );
 
+    const getStopItems = (): ReactElement[] => {
+        // console.log('selectedValueType', optionsToPopulate);
+        // console.log('is auto popoulate', isAutoPopulate);
+        // console.log('selected farestages', selectedFareStages);
+        const stopItems: ReactElement[] = stops.map((stop, index) => {
+            let selectValue = '';
+
+            if (isAutoPopulate) {
+                if (selectedOption.length === 1) {
+                    if (index > parseInt(selectedOption[0].position, 10)) {
+                        const stageName = JSON.parse(selectedOption[0].value).stage;
+                        selectValue = JSON.stringify({ stop, stage: stageName });
+                        console.log('select value if auto===', selectValue);
+                    }
+                }
+            }
+
+            if (!isAutoPopulate) {
+                userFareStages.fareStages.map((stage: FareStage) => {
+                    const currentValue = JSON.stringify({ stop, stage: stage.stageName });
+
+                    const isSelected = selectedFareStages.some(selectedObject => {
+                        return selectedObject === currentValue;
+                    });
+
+                    if (isSelected) {
+                        selectValue = currentValue;
+                    }
+
+                    return null;
+                });
+            }
+
+            return (
+                <tr key={stop.atcoCode} className="govuk-table__row">
+                    <td className="govuk-table__cell stop-cell" id={`stop-${index}`}>
+                        {formatStopName(stop)}
+                    </td>
+                    <td className="govuk-table__cell naptan-cell" id={`naptan-${index}`}>
+                        {stop.naptanCode}
+                    </td>
+                    <td className="govuk-table__cell stage-cell">
+                        {/* eslint-disable-next-line jsx-a11y/no-onchange */}
+                        <select
+                            className="govuk-select farestage-select"
+                            id={`option-${index}`}
+                            name={`option-${index}`}
+                            defaultValue={selectValue}
+                            aria-labelledby={`stop-name-header stop-${index} naptan-code-header naptan-${index}`}
+                            onChange={selectedValueCallback}
+                        >
+                            <option value="notApplicable">Not Applicable</option>
+                            {userFareStages.fareStages.map((stage: FareStage) => {
+                                return (
+                                    <option
+                                        key={stage.stageName}
+                                        //selected={selectValue === JSON.stringify({ stop, stage: stage.stageName })}
+                                        value={JSON.stringify({ stop, stage: stage.stageName })}
+                                    >
+                                        {stage.stageName}
+                                    </option>
+                                );
+                            })}
+                        </select>
+                    </td>
+                </tr>
+            );
+        });
+        return stopItems;
+    };
+
     if (error) {
         errors.push({ errorMessage: 'Ensure each fare stage is assigned at least once.', id: 'option-0' });
     }
@@ -103,14 +178,34 @@ const MatchingBase = ({
                                 {travelineHintText}
                             </span>
                             <FormElementWrapper errors={errors} errorId="option-0" errorClass="">
-                                <MatchingList
-                                    userFareStages={userFareStages}
-                                    stops={stops}
-                                    selectedFareStages={selectedFareStages}
-                                    selectOptionCallback={selectedValue}
-                                    optionsToPopulate={selectedOption}
-                                    isAutoPopulate={isAutoPopulate}
-                                />
+                                <table className="govuk-table">
+                                    <thead className="govuk-table__head">
+                                        <tr className="govuk-table__row">
+                                            <th
+                                                scope="col"
+                                                className="govuk-table__header govuk-!-width-one-half"
+                                                id="stop-name-header"
+                                            >
+                                                Stop name
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                className="govuk-table__header govuk-!-width-one-quarter"
+                                                id="naptan-code-header"
+                                            >
+                                                Naptan code
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                className="govuk-table__header govuk-!-width-one-quarter"
+                                                id="fare-stage-header"
+                                            >
+                                                Fare stage
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="govuk-table__body">{getStopItems()}</tbody>
+                                </table>
                             </FormElementWrapper>
                         </fieldset>
                     </div>
@@ -118,7 +213,7 @@ const MatchingBase = ({
                     <input type="hidden" name="service" value={JSON.stringify(service)} />
                     <input type="hidden" name="userfarestages" value={JSON.stringify(userFareStages)} />
                     <input type="submit" value="Continue" id="submit-button" className="govuk-button" />
-                    <button className="govuk-link" type="button" onClick={handleClick}>
+                    <button className="govuk-button govuk-!-margin-left-3" type="button" onClick={handleClick}>
                         Auto-complete remaining fare stages
                     </button>
                 </>
